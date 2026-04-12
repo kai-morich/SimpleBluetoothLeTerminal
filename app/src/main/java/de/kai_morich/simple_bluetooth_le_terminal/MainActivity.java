3e46f96522ee3b56d946cf2965e6140192b052f5
Kai Morich <mail@kai-morich.de>
2026-04-12 09:21:42 +0200

    api 36 + compensate edge-to-edge enforcement

--- app/src/main/java/de/kai_morich/simple_bluetooth_le_terminal/MainActivity.java
+++ app/src/main/java/de/kai_morich/simple_bluetooth_le_terminal/MainActivity.java
@@ -1,9 +1,13 @@
 package de.kai_morich.simple_bluetooth_le_terminal;
 
 import android.os.Bundle;
-import androidx.fragment.app.FragmentManager;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity implements FragmentManager.OnBackStackChangedListener {
 
@@ -11,6 +15,12 @@ public class MainActivity extends AppCompatActivity implements FragmentManager.O
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            Insets ime = insets.getInsets(WindowInsetsCompat.Type.ime());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, Math.max(systemBars.bottom, ime.bottom));
+            return insets;
+        });
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportFragmentManager().addOnBackStackChangedListener(this);
